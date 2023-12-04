e1d466f3c1ef0893c5b812716bf4fcc5b801279d
Dungcannon <gavinramos008@gmail.com>
2023-12-03 20:58:49 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,225 @@
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+
+#include "vex.h"
+
+using namespace vex;
+competition Competition;
+
+// Brain should be defined by default
+brain Brain;
+
+
+// START V5 MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+
+// Robot configuration code.
+motor LeftDriveSmart = motor(PORT1, ratio18_1, false);
+motor RightDriveSmart = motor(PORT2, ratio18_1, true);
+inertial DrivetrainInertial = inertial(PORT3);
+smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 319.19, 320, 40, mm, 1);
+
+motor Harvester = motor(PORT4, ratio18_1, false);
+
+motor Catapult = motor(PORT5, ratio18_1, false);
+
+motor Frills = motor(PORT6, ratio18_1, false);
+
+controller Controller1 = controller(primary);
+
+void calibrateDrivetrain() {
+  wait(200, msec);
+  Brain.Screen.print("Calibrating");
+  Brain.Screen.newLine();
+  Brain.Screen.print("Inertial");
+  DrivetrainInertial.calibrate();
+  while (DrivetrainInertial.isCalibrating()) {
+    wait(25, msec);
+  }
+
+  // Clears the screen and returns the cursor to row 1, column 1.
+  Brain.Screen.clearScreen();
+  Brain.Screen.setCursor(1, 1);
+}// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool Controller1LeftShoulderControlMotorsStopped = true;
+bool Controller1RightShoulderControlMotorsStopped = true;
+bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3
+      // right = Axis2
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position();
+      int drivetrainRightSideSpeed = Controller1.Axis2.position();
+      
+      // check if the value is inside of the deadband range
+      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
+        // check if the left motor has already been stopped
+        if (DrivetrainLNeedsToBeStopped_Controller1) {
+          // stop the left drive motor
+          LeftDriveSmart.stop();
+          // tell the code that the left motor has been stopped
+          DrivetrainLNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
+        DrivetrainLNeedsToBeStopped_Controller1 = true;
+      }
+      // check if the value is inside of the deadband range
+      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
+        // check if the right motor has already been stopped
+        if (DrivetrainRNeedsToBeStopped_Controller1) {
+          // stop the right drive motor
+          RightDriveSmart.stop();
+          // tell the code that the right motor has been stopped
+          DrivetrainRNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
+        DrivetrainRNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainLNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainRNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+      // check the ButtonL1/ButtonL2 status to control Harvester
+      if (Controller1.ButtonL1.pressing()) {
+        Harvester.spin(forward);
+        Controller1LeftShoulderControlMotorsStopped = false;
+      } else if (Controller1.ButtonL2.pressing()) {
+        Harvester.spin(reverse);
+        Controller1LeftShoulderControlMotorsStopped = false;
+      } else if (!Controller1LeftShoulderControlMotorsStopped) {
+        Harvester.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1LeftShoulderControlMotorsStopped = true;
+      }
+      // check the ButtonR1/ButtonR2 status to control Catapult
+      if (Controller1.ButtonR1.pressing()) {
+        Catapult.spin(forward);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (Controller1.ButtonR2.pressing()) {
+        Catapult.spin(reverse);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (!Controller1RightShoulderControlMotorsStopped) {
+        Catapult.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1RightShoulderControlMotorsStopped = true;
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+
+
+
+#pragma endregion VEXcode Generated Robot Configuration
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       {Ramos}                                                  */
+/*    Created:      {12/1}                                                    */
+/*    Description:  Testing and producing an autonomous program                                           */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// Include the V5 Library
+#include "vex.h"
+#include <math.h> 
+#include "MiniPID.h"
+ 
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+
+bool getHappy = false;
+void triggerHappy(int timems){
+  getHappy = true;
+  int i; //counter
+  while (getHappy == true){
+    {
+      if (i>=timems) {getHappy = false;} //if counter more than ms
+      Catapult.spin(forward);
+      wait(10, msec);
+      i+=10;
+    }
+  }
+}
+
+//Power ratio - convert degrees to volts.
+//PID
+/* MiniPID pid;
+pid = MiniPID(1,0,0); // for PID, needs a library */
+
+double ratio = 0.1; // temp ratio val
+void proportionalTurnR(int x){  //power ratio - convert degrees to volts
+  double destination = x;
+  double error;
+  error = destination-DrivetrainInertial.rotation(degrees);
+  LeftDriveSmart.spin(forward, error*ratio, volt);
+  RightDriveSmart.spin(reverse, error*ratio, volt);
+
+}
+void proportionalTurnL(int x){  //power ratio - convert degrees to volts
+  double destination = x;
+  double error;
+  error = destination-DrivetrainInertial.rotation(degrees);
+  LeftDriveSmart.spin(reverse, error*ratio, volt);
+  RightDriveSmart.spin(forward, error*ratio, volt);
+}
+/*void turnToP(int x){
+  //GET SENSOR
+  //SET SOME SORT TARGET
+  double output=pid.getOutput(sensor, target);
+  // do something with it 
+  delay(50);
+} base most barebones PID*/
+
+//without PID
+void Forward(int x){
+  Drivetrain.driveFor(forward, x, mm);
+  
+}
+void TurnTo(int x){
+  Drivetrain.turnToHeading(x, degrees);
+}
+
+int main() {
+  calibrateDrivetrain();
+  proportionalTurnR(90); // attempts to turn right 
+  Forward(1000); // 1meter forward
+}
